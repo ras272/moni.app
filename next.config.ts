e8e3553b4972939880d01c4b537897d4a49d42e0
ras272 <rashacking1@gmail.com>
2025-11-03 00:24:31 -0300

    fix: Remover opciones no soportadas de PWA config

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const withPWA = withPWAInit({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  skipWaiting: true,
-  reloadOnOnline: true,
   workboxOptions: {
     disableDevLogs: true
   }
